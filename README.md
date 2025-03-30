3c81707f8e082120d1522e85a3e5fa5d586625c7
Dima <defire04st@gmail.com>
2025-03-30 04:13:15 +0300

    add docker-compose.yml

--- README.md
+++ README.md
@@ -41,7 +41,7 @@ quad-bike-shop-aiogram/
 ├── requirements.txt
 ├── Dockerfile
 ├── data/
-│   ├── config.origin
+│   ├── config.py
 │   └── db.py
 ├── repositories/
 │   ├── product_repository.py
---
+++ data/config.py
@@ -0,0 +1,7 @@
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN", "")
+DB_PATH = os.getenv("DB_PATH")
+ADMIN_IDS = [int(os.getenv("ADMIN_IDS"))] if os.getenv("ADMIN_IDS") else []
+PAYMENT_TOKEN = os.getenv("PAYMENT_TOKEN", "")
+
