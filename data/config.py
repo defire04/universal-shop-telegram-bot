3e2decc81cf0e182d7d37f6b316ccf4b7641e39d
Dima <defire04st@gmail.com>
2025-04-07 18:06:18 +0300

    add AI assistant

--- data/config.py
+++ data/config.py
@@ -1,12 +1,13 @@
 import os
+from dotenv import load_dotenv
 
+load_dotenv()
 
-
-BOT_TOKEN = os.getenv("BOT_TOKEN", "")
+BOT_TOKEN = os.getenv("QUAD_BOT_TOKEN", "")
 DB_PATH = os.getenv("DB_PATH") if os.getenv("DB_PATH") else "data/database2.db"
 
-ADMIN_IDS = [int(os.getenv("ADMIN_IDS"))] if os.getenv("ADMIN_IDS") else [int("")]
-PAYMENT_TOKEN = os.getenv("PAYMENT_TOKEN", "")
-
+admin_ids_str = os.getenv("ADMIN_IDS", "")
+ADMIN_IDS = [int(id_str) for id_str in admin_ids_str.split(",") if id_str] if admin_ids_str else []
+PAYMENT_TOKEN = os.getenv("QUAD_PAYMENT_TOKEN", "")
 
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY", "")
